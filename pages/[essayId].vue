ad9f28ba32c37e8d10034583d703d094429df3e2
Prabin Acharya <acharyaprabin101@gmail.com>
2023-12-04 01:46:31 +0545

    add comments works!!!!

--- pages/[essayId].vue
+++ pages/[essayId].vue
@@ -9,7 +9,41 @@
       <div v-if="!fetchingListInfo" class="text-left">
         <div class="pb-8">
           <h2 class="text-5xl font-bold mb-4">{{ essay.title }}</h2>
-          <p class="text-lg">{{ essay.content }}</p>
+          <!-- <p class="text-lg">{{ essay.content }}</p> -->
+          <div
+            v-for="(paragraph, index) in essay.content.split('\n')"
+            :key="index"
+            class="mb-4 text-lg hover:cursor"
+            @click="showCommentInput(index)"
+            :style="{ cursor: getParagraphCursor(index) }"
+          >
+            <p>
+              {{ paragraph }}
+            </p>
+            <div v-if="isInputVisible(index)" class="flex flex-row-reverse">
+              <div
+                class="mt-1 border-2 border-purple-600 rounded-full p-2 w-fit bg-black text-white py-3"
+              >
+                <input
+                  autofocus
+                  type="text"
+                  placeholder="Add comment..."
+                  class="outline-none font-bold bg-black px-2"
+                  @blur="hideCommentInput"
+                  @keydown.enter="saveComment(index, $event)"
+                />
+              </div>
+            </div>
+            <div v-if="commentsByParagraph[index]">
+              <div
+                v-for="(comment, commentIndex) in commentsByParagraph[index]"
+                :key="commentIndex"
+                class="w-fit p-2 rounded-full bg-gray-900 text-white px-6 shadow-2xl"
+              >
+                {{ comment }}
+              </div>
+            </div>
+          </div>
         </div>
         <br />
         <p><b>Written by: </b> {{ essay.author?.substr(0, 22) }}...</p>
@@ -48,7 +82,7 @@
       </ul>
 
       <!-- Add Comment Form -->
-      <div class="mt-4 mb-5">
+      <!-- <div class="mt-4 mb-5">
         <form class="flex space-x-4" @submit.prevent="addComment">
           <div class="border-b border-gray-200 sm:w-full">
             <label for="add-todo" class="sr-only">Add a comment</label>
@@ -69,7 +103,7 @@
             <PlusIconMini class="h-5 w-5" aria-hidden="true" />
           </button>
         </form>
-      </div>
+      </div> -->
     </div>
     <div class="mt-4">
       <nuxt-link to="/" class="text-blue-500">Back to Essay Lists</nuxt-link>
@@ -145,16 +179,28 @@ onBeforeMount(async () => {
   fetchingListInfo.value = false;
 });
 
-async function addComment() {
+const showAddCommentInput = ref(null);
+const commentsByParagraph = ref({});
+
+// let comments = ref([]);
+
+const showCommentInput = (index) => {
+  showAddCommentInput.value = index;
+};
+
+const hideCommentInput = () => {
+  showAddCommentInput.value = null;
+};
+
+async function saveComment(index, event) {
   const commentData = {
-    content: newCommentContent.value,
+    content: event.target.value,
+    paragraphIndex: index,
     author: myDID,
     parentId: essayId.value,
   };
 
   newCommentContent.value = "";
-
-  console.log(commentData);
 
   const { record: commentRecord, status: createStatus } =
     await web5.dwn.records.create({
@@ -173,7 +219,7 @@ async function addComment() {
   const comment = { commentRecord, data, id: commentRecord.id };
   commentItems.value.push(comment);
 
-  const { status: sendStatus } = await commentItems.send(commentRecipient);
+  const { status: sendStatus } = await commentRecord.send(commentRecipient);
 
   if (sendStatus.code !== 202) {
     console.log("Unable to send to target did:" + sendStatus);
@@ -181,5 +227,54 @@ async function addComment() {
   } else {
     console.log("Sent todo to recipient");
   }
+
+  hideCommentInput();
 }
+
+const isHovered = (index) => {
+  return showAddCommentInput.value !== index;
+};
+
+const getParagraphCursor = (index) =>
+  isInputVisible(index) ? "auto" : "pointer";
+
+const isInputVisible = (index) => showAddCommentInput.value === index;
+
+// async function addComment() {
+//   const commentData = {
+//     content: newCommentContent.value,
+//     author: myDID,
+//     parentId: essayId.value,
+//   };
+
+//   newCommentContent.value = "";
+
+//   console.log(commentData);
+
+//   const { record: commentRecord, status: createStatus } =
+//     await web5.dwn.records.create({
+//       data: commentData,
+//       message: {
+//         protocol: protocolDefinition.protocol,
+//         protocolPath: "essay/comment",
+//         schema: protocolDefinition.types.comment.schema,
+//         dataFormat: protocolDefinition.types.comment.dataFormats[0],
+//         parentId: essayId.value,
+//         contextId: essayId.value,
+//       },
+//     });
+
+//   const data = await commentRecord.data.json();
+//   const comment = { commentRecord, data, id: commentRecord.id };
+//   commentItems.value.push(comment);
+
+//   const { status: sendStatus } = await commentRecord.send(commentRecipient);
+
+//   if (sendStatus.code !== 202) {
+//     console.log("Unable to send to target did:" + sendStatus);
+//     return;
+//   } else {
+//     console.log("Sent todo to recipient");
+//   }
+// }
 </script>
